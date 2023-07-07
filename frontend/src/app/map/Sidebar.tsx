28751ea86f0b004df4f38e2c7cc3bbc3abc98429
Japsert <40762304+Japsert@users.noreply.github.com>
2023-07-07 17:10:40 +0200

    rename CollapseSidebarComponent

--- frontend/src/app/map/Sidebar.tsx
+++ frontend/src/app/map/Sidebar.tsx
@@ -1,6 +1,6 @@
 import DataArtist from "@/utils/dataArtist";
+import CollapseSidebarButton from "./CollapseSidebarButton";
 import SidebarContent from "./SidebarContent";
-import CollapseSidebarComponent from "./CollapseSidebarComponent";
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -16,8 +16,11 @@ export default function Sidebar({
   return (
     <div id="sidebar" className={`w-full h-1/2 md:w-96 md:h-full`}>
       <SidebarContent dataArtist={dataArtist} />
-      
-      <CollapseSidebarComponent sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+      <CollapseSidebarButton
+        sidebarOpen={sidebarOpen}
+        toggleSidebar={toggleSidebar}
+      />
     </div>
   );
 }
