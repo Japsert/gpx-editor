42b48218f02eb9a5c8db2cb2b4b441ab095db1a1
Japsert <40762304+Japsert@users.noreply.github.com>
2023-07-15 15:39:31 +0200

    add test page (for testing css stuff)

---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,15 @@
+export default function TestPage() {
+  return (
+    <div className="w-[100px] h-[100%*1.5] bg-sky-50 overflow-scroll">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
+}
