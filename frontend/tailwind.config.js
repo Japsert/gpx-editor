f12ac79ec9dc5648fe1a3753814641bcc50c283e
Japsert <40762304+Japsert@users.noreply.github.com>
2023-07-15 15:39:45 +0200

    add custom screen width to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
     fontFamily: {
       mono: ["monospace"],
     },
+    screens: {
+      "448px": "448px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
