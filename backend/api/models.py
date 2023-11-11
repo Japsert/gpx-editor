94f41296003dbc830be83bc6dd57b2fcb8d687cc
Japsert <40762304+Japsert@users.noreply.github.com>
2023-11-11 18:42:06 +0100

    Add GeoJsonData model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from django.forms import ValidationError
 
 
 class Hello(models.Model):
     text = models.TextField()
+
+
+class GeoJsonData(models.Model):
+    date = models.DateField(unique=True)
+    data = models.JSONField()
+
+    def clean(self):
+        if GeoJsonData.objects.filter(date=self.date).exists():
+            raise ValidationError("GeoJsonData with this date already exists")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
